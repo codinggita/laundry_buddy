a695d4b1faf0ea8ce99e3dfe37aa5405280f119e
mayur waykar <mayurwaykar9@gmail.com>
2025-01-31 11:09:03 +0530

    Refactored project structure for better organization

--- BACKEND/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser,loginUser } = require('../controllers/userController');
-
-// Signup route
-router.post('/signup', registerUser);
-router.post('/login', loginUser);
-
-module.exports = router;
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB =require('./config/db');
-const orderRoutes = require('./routes/orderRoutes')
+const orderRoutes = require('./routes/userRoutes/orderRoutes')
+const userRoutes = require('./routes/userRoutes/userRoute')
+const complaintRoutes = require('./routes/userRoutes/complaintRoutes/complaintRoutes')
 const cors = require('cors'); // Importing cors
 
 const app = express();
@@ -13,7 +15,8 @@ app.use(cors()); // Enabling CORS for all routes
 // connect to database
 connectDB();
 
-app.use('/user',orderRoutes)
+app.use('/user',orderRoutes,userRoutes,complaintRoutes)
+
 
 app.listen(port, () => {
   console.log(`Server start on http://localhost:${port}`);
