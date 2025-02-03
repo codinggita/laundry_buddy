22d46b3589ce51a438f30710ffc306991df06f37
mayur waykar <mayurwaykar9@gmail.com>
2025-02-03 16:01:24 +0530

    Create reusable card component with responsive style

--- Frontend/src/Component/User/Card.jsx
+++ Frontend/src/Component/User/Card.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const Card = ({ title, value, icon, bgColor, textColor }) => {
   return (
-    <div className={`p-4 rounded-lg shadow-md ${bgColor} ${textColor}`}>
-      <div className="flex items-center justify-between">
-        <div>
-          <h4 className="text-base items-center font-semibold">{title}</h4>
-          <p className="text-xl font-bold">{value}</p>
-        </div>
-        <div className="text-3xl">{icon}</div>
+    <div className={`p-4 rounded-lg  shadow-md ${bgColor} ${textColor} flex flex-col items-center`}>
+      {/* Icon at the top */}
+      <div className="flex justify-center mb-2">
+        {icon}
       </div>
+
+      {/* Title in the center */}
+      <h4 className="text-base font-semibold text-center mb-2">{title}</h4>
+
+      {/* Value at the bottom */}
+      <p className="text-xl font-bold">{value}</p>
     </div>
   );
 };
