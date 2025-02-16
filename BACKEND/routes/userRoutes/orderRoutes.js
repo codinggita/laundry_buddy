086be54a36c73d6e57f4fc30c99e017b8aa1985b
mayur waykar <mayurwaykar9@gmail.com>
2025-02-16 23:04:17 +0530

    🗂️ Added /order-history route to fetch user's order summary with authentication

--- BACKEND/routes/userRoutes/orderRoutes.js
+++ BACKEND/routes/userRoutes/orderRoutes.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const authenticateUser = require("../../middleware/authMiddleware")
-const {submitOrder,getAllOrdersAndTotalNumber,getAllPendingOrdersAndTotalNumber,getAllCompletedOrdersAndTotalNumber,deleteOrderBybagNumber} = require('../../controllers/user/OrderManagement/orderController')
+const {submitOrder,getOrderSummary} = require('../../controllers/user/OrderManagement/orderController')
+const authMiddleware = require('../../middleware/authMiddleware');
 
 
 router.post('/submit-order',authenticateUser,submitOrder);
-router.get('/all-orders' ,getAllOrdersAndTotalNumber)
-router.get('/pending-orders' ,getAllPendingOrdersAndTotalNumber)
-router.get('/completed-orders' ,getAllCompletedOrdersAndTotalNumber)
-  router.delete('/delete-order/:bagNumber',deleteOrderBybagNumber)
-
+router.get('/order-history',authMiddleware ,getOrderSummary);
 
 module.exports = router
