6832758c80a3dc01e3f511b2c5ee697bcd3f32fc
mayur waykar <mayurwaykar9@gmail.com>
2025-02-17 16:48:15 +0530

    Wrapp the App.jsx in OrderProvider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,12 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import { OrderProvider } from './Component/User/SubmitOrder/OrderContext'
 import App from './App'
 createRoot(document.getElementById('root')).render(
+<OrderProvider>
 
     <App />
+</OrderProvider>
   
 )
