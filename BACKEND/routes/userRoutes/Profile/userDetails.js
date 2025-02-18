3692baa5bc6086ef1dd4e6df626a0922f0b3461b
mayur waykar <mayurwaykar9@gmail.com>
2025-02-18 14:57:07 +0530

    feat: Add profile update route with authentication middleware
    - Added a PATCH route at '/profile' to handle user profile updates.
    - Applied 'authMiddleware' to ensure the user is authenticated before updating their profile.
    - Linked the 'updateUserProfile' function as the route handler.

--- BACKEND/routes/userRoutes/Profile/userDetails.js
+++ BACKEND/routes/userRoutes/Profile/userDetails.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const authMiddleware =  require('../../../middleware/authMiddleware');
-const {getUserProfile} = require('../../../controllers/user/Profile-Management/profileController')
+const {getUserProfile,updateUserProfile} = require('../../../controllers/user/Profile-Management/profileController')
 const router = express.Router();
 
 router.get('/profile',authMiddleware,getUserProfile);
+router.patch('/profile',authMiddleware,updateUserProfile);
 
 module.exports = router
