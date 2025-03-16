c2f7bcde855e95c98151c5d7224203526d71d35f
mayur2410-tech <mayurwaykar9@gmail.com>
2025-03-16 11:37:26 +0530

    Update Submitorder.jsx

--- Frontend/src/Component/User/SubmitOrder/Submitorder.jsx
+++ Frontend/src/Component/User/SubmitOrder/Submitorder.jsx
@@ -143,7 +143,7 @@ export default function SubmitOrder() {
 
   // Handling socket.io connection 
   useEffect(() => {
-    const socketConnection = io('http://localhost:8080'); // Create the socket inside useEffect
+    const socketConnection = io('https://laundry-buddy-yysq.onrender.com'); // Create the socket inside useEffect
     setSocket(socketConnection);
 
     socketConnection.on('connect', () => {
