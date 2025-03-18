fe6807866510ab5c415146f83b0c01e79b041948
mayur2410-tech <mayurwaykar9@gmail.com>
2025-03-18 18:10:51 +0530

    Fix the input type of date

--- Frontend/src/Component/Worker/Dashboard/NewOrder.jsx
+++ Frontend/src/Component/Worker/Dashboard/NewOrder.jsx
@@ -86,11 +86,11 @@ function NewOrder({isOpen,onClose}) {
               <div className="relative">
                 <Calendar className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
                 <input
-                  type="text"
+                  type="date"
                   className="w-full pl-10 pr-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                   placeholder="dd-mm-yyyy"
                 />
-                <Calendar className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
+                {/* <Calendar className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" /> */}
               </div>
             </div>
 
